da1f5c695c48ed4a04aae3d158455a1e0ca92e5d
Leandro <leoooquipildor@gmail.com>
2025-11-24 15:27:50 -0300

    Feat: cree un modelo de productos

---
+++ server/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'El nombre es obligatorio'],
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'La descripción es obligatoria']
+  },
+  price: {
+    type: Number,
+    required: [true, 'El precio es obligatorio'],
+    min: 0
+  },
+  image: {
+    type: String,
+    required: [true, 'La imagen es obligatoria'], 
+    // Por ahora guardaremos URLs, luego usaremos Cloudinary
+  },
+  category: {
+    type: String,
+    required: [true, 'La categoría es obligatoria'],
+    enum: ['Ropa', 'Electrónica', 'Hogar', 'Accesorios', 'Otros'] // Puedes cambiar esto según tu nicho
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false // Para mostrar en el Home como "Destacado"
+  }
+}, {
+  timestamps: true // Esto crea automáticamente campos createdAt y updatedAt
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
