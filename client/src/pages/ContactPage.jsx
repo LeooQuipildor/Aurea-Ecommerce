e24c5482c945a092d1aa4f268d520c94f1fc79f0
Leandro <leoooquipildor@gmail.com>
2025-12-17 23:48:35 -0300

    Actualizada toda la informacion de contacto: telefono +57 321 842 2436 y ubicacion Colombia

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -91,7 +91,7 @@ const ContactPage = () => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </svg>
-              <span className="text-sm text-white">+54 11 1234-5678</span>
+              <span className="text-sm text-white">+57 321 842 2436</span>
             </div>
 
             <div className="flex items-center gap-2">
@@ -108,7 +108,9 @@ const ContactPage = () => {
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 />
               </svg>
-              <span className="text-sm text-white">contacto@aurea.com.ar</span>
+              <span className="text-sm text-white">
+                aurea.co.store@gmail.com
+              </span>
             </div>
 
             <div className="flex items-center gap-2">
@@ -131,9 +133,7 @@ const ContactPage = () => {
                   d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
-              <span className="text-sm text-white">
-                Buenos Aires, Argentina
-              </span>
+              <span className="text-sm text-white">Colombia</span>
             </div>
           </div>
 
